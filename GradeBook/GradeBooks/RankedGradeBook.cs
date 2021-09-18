c4dba2c9a80c6ed843d283c1e03f051a0d78acdd
Donny Thompson <UT2315@corp.Alfacompanies.com>
2021-09-18 06:57:03 -0500

    mod 3

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -22,7 +22,14 @@ namespace GradeBook.GradeBooks
             var threshold = (int)Math.Ceiling(Students.Count * .2);
             var grades = Students.OrderBy(e => e.AverageGrade).Select(e => e.AverageGrade).ToList();
 
-            if()
+            if (averageGrade >= grades[threshold - 1])
+                return 'A';
+            if(averageGrade>= grades[threshold*2]-1)
+                return 'B';
+            if (averageGrade >= grades[threshold * 3] - 1)
+                return 'C';
+            if (averageGrade >= grades[threshold * 4] - 1)
+                return 'D';
             return 'F';
         }
     }
