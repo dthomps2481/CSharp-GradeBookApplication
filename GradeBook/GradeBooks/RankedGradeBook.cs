2bee32679b6eb2507a23c6118afa60efa51965ac
Donny Thompson <UT2315@corp.Alfacompanies.com>
2021-09-18 06:48:10 -0500

    yes

---
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GradeBook.GradeBooks
+{
+    class RankedGradeBook: BaseGradeBook
+    {
+        public RankedGradeBook(string name): base(name)
+        {
+            Type = Enums.GradeBookType.Ranked;
+        }
+        public override char GetLetterGrade(double averageGrade)
+        {
+            if(Students.Count < 5)
+            {
+                throw new InvalidOperationException("Ranked grading requires 5 students at minimum.");
+            }
+
+            var threshold = (int)Math.Ceiling()
+            return 'F';
+        }
+    }
+}
